e6607639859e3a8539219e187161204f442f4aea
Arnav <arnav.mach@gmail.com>
2021-03-20 18:20:21 -0500

    Updated GME Stock Data up to March 18

--- get_percent_change.py
+++ get_percent_change.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pdb
 import numpy as np
 
-gme_stock = pd.read_csv('Data/archive_4/GME_price_updated.csv')
+gme_stock = pd.read_csv('Data/archive_7/GME_stock_updatedmarch.csv')
 gme_percentage_change = []
 for i in range(gme_stock.shape[0]-1):
 	close_price_today = gme_stock.loc[i, 'Close']
@@ -12,6 +12,5 @@ for i in range(gme_stock.shape[0]-1):
 gme_percentage_change.append(np.nan)
 gme_stock = gme_stock.rename(columns={'Date': 'date'})
 gme_stock['gme_percentage_change'] = gme_percentage_change
-pdb.set_trace()
 gme_stock = gme_stock.loc[gme_stock.isna().sum(axis=1)==0,:]
 gme_stock.to_csv('Data/percentage_change_by_day.csv', index = False)
